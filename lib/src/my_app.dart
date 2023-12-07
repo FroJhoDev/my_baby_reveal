92508e7b2f836d32ab48e86c56d74e710069021e
Jonathan Willian <jonathan1313>
2023-12-07 12:28:10 -0300

    feat: added conditional to different home page based on flavor

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/styles/app_theme.dart';
+import 'core/flavors/flavor_config.dart';
 import 'core/providers/app_providers.dart';
 
 import 'features/voting/pages/home_page_family/home_page_family.dart';
+import 'features/voting/pages/home_page_parents/home_page_parents.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +19,9 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'My Baby Reveal',
         theme: AppTheme.themeData,
-        home: const HomePageFamily(),
+        home: FlavorConfig.shared.flavor == FlavorProducts.family
+            ? const HomePageFamily()
+            : const HomePageParents(),
       ),
     );
   }

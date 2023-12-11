5785b3ec8f6022004c8b4c5bd4ce741efe7dfd5a
Jonathan Willian <jonathan1313>
2023-12-11 10:12:04 -0300

    refactor: remove not used import

--- lib/src/features/voting/pages/home_page_parents/home_page_parents.dart
+++ lib/src/features/voting/pages/home_page_parents/home_page_parents.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_baby_reveal/src/core/extensions/size_extension.dart';
 
